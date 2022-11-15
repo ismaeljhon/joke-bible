bda895c2b740311ae9c2c6ec9bf454be50cecd16
Ismael Jalbuna <jalbuna.ismael@gmail.com>
2022-11-15 23:20:26 +0800

    Add saving of joke item for likes/dislikes functionality to be saved in localstorage

--- src/services/JokesService.js
+++ src/services/JokesService.js
@@ -1,4 +1,5 @@
 import ChuckNorrisApiService from "./ChuckNorrisApiService";
+import LocalStorageService from "./LocalStorageService";
 export default class JokesService extends ChuckNorrisApiService {
   constructor() {
     super({
@@ -23,5 +24,28 @@ export default class JokesService extends ChuckNorrisApiService {
 
   async search (searchString = '') {
 
+  }
+
+  /** Save Item to local storage */
+  async saveItem (item) {
+    const localStorageService = new LocalStorageService('jokes')
+    const currentJokes = localStorageService.get()
+
+    const { result = [], total = 0 } = currentJokes
+
+    // save the item to local storage
+    localStorageService.save({
+      total,
+      result: result.map(joke => {
+        if (item.id === joke.id) {
+          return {
+            ...joke,
+            ...item
+          }
+        }
+
+        return joke
+      })
+    })
   }
 }
