64ab037cbaef91afe23e07e39d0f4f05cdfe3b44
Ismael Jalbuna <jalbuna.ismael@gmail.com>
2022-11-15 13:04:53 +0800

    Create skeleton layout for the first layout of the homepage - joke list in category view

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <a>So Funktioniert's</a>
+          <a>Sonderangebote</a>
+          <a>Mein Bereich</a>
+        </nav>
       </header>
+
+      <main>
+        <section className='main-banner'>
+          <h1>The Joke Bible</h1>
+          <h5>Daily Laughs for you and yours</h5>
+
+          <div className='search-bar'>
+            <input type="search" placeholder='How can we make you laugh today?' />
+          </div>
+        </section>
+
+        <section className='category-filter-list'>
+          <ul>
+            <li>
+              <a>Adult Jokes</a>
+            </li>
+            <li>
+              <a>Dad Jokes</a>
+            </li>
+            <li>
+              <a>Christmas Jokes</a>
+            </li>
+            <li>
+              <a>Job Jokes</a>
+            </li>
+            <li>
+              <a>Birthday Jokes</a>
+            </li>
+            <li>
+              <a>Social Jokes</a>
+            </li>
+            <li>
+              <a>Puns Jokes</a>
+            </li>
+            <li>
+              <a>View All</a>
+            </li>
+          </ul>
+        </section>
+
+        <section className='joke-list'>
+          <aside className='filterSelected'>
+            <ul>
+              <li>Social Jokes</li>
+            </ul>
+          </aside>
+
+          <article>
+            <h3>Lawyer Joke</h3>
+            <p>Chuck Norris invented the bolt-action rifle, liquor, sexual intercourse, and football-- in that order.</p>
+            <a href="">See more</a>
+          </article>
+
+          <article>
+            <h3>Doctor Joke</h3>
+            <p>The Chuck Norris facts game is played out... And we should all stop contributing to this stupid shit! Unless your a dickless gamer that dwells on this Chuck Norris nonsense</p>
+            <a href="">See more</a>
+          </article>
+
+          <article>
+            <h3>Business Joke</h3>
+            <p>Chuck Norris invented the bolt-action rifle, liquor, sexual intercourse, and football-- in that order.</p>
+            <a href="">See more</a>
+          </article>
+
+          <article>
+            <h3>Police Joke</h3>
+            <p>Chuck Norris invented the bolt-action rifle, liquor, sexual intercourse, and football-- in that order.</p>
+            <a href="">See more</a>
+          </article>
+
+          <aside>
+            <a>View All</a>
+          </aside>
+        </section>
+      </main>
     </div>
   );
 }
